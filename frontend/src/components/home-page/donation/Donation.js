e9879d11fa7ceb9fa430de43808762a0f21a14a2
Patrycja Kalita <pati.kalita@gmail.com>
2022-10-22 13:37:59 +0200

    Frontend: Donation part
    Signed-off-by: Patrycja Kalita <pati.kalita@gmail.com>

--- frontend/src/components/home-page/donation/Donation.js
+++ frontend/src/components/home-page/donation/Donation.js
@@ -1,27 +1,35 @@
 import React from 'react';
 
 import './Style.css'
-import image from '../../../assets/Logo.png';
+import cat from '../../../assets/donation/cat.png';
+import dog from '../../../assets/donation/dog.png';
+import bird from '../../../assets/donation/bird.png';
+import rabbit from '../../../assets/donation/rabbit.png';
+import turtle from '../../../assets/donation/turtle.png';
 
 const Donation = () => {
     return (
-        <div className="">
-            <div className="">
-                <h1 className="">Chcesz nam pomóc?</h1>
-                <div className="">
-                    <span className="">Wpłać dowolną darowiznę:</span><br/>
-                    <span className="">93 3888 3099 3322 0001 9320 0029</span><br/>
-                    <span className="">Tytułem: Darowizna - House of pets</span>
+        <div className="mb-32 justify-center flex main-container">
+            <div className="container">
+                <h1 className="top-text">Chcesz nam pomóc?</h1>
+
+                <div className="flex donation-container">
+                <div className="left-container">
+                    <span className="description-text">Wpłać dowolną darowiznę:</span>
+                    <h1 className="number-text">93 3888 3099 3322 0001 9320 0029</h1>
+                    <span className="description-text">Tytułem: Darowizna - House of pets</span>
                 </div>
-                <div className="">
-                    <span className="">lub przy użyciu PayPal: informacja@hosueofpets.pl</span>
-                    <div className="">
-                        <img alt="cat" src={image} className=""/>
-                        <img alt="dog" src={image} className=""/>
-                        <img alt="bird" src={image} className=""/>
-                        <img alt="rabbit" src={image} className=""/>
-                        <img alt="turtle" src={image} className=""/>
+
+                <div className="right-container">
+                    <span className="description-text">lub przy użyciu PayPal: informacja@hosueofpets.pl</span>
+                    <div className="img-container">
+                        <img alt="cat" src={cat} className="animal-img mr-5"/>
+                        <img alt="dog" src={dog} className="animal-img mr-5"/>
+                        <img alt="bird" src={bird} className="animal-img mr-5"/>
+                        <img alt="rabbit" src={rabbit} className="animal-img mr-5"/>
+                        <img alt="turtle" src={turtle} className="animal-img mr-5"/>
                     </div>
+                </div>
                 </div>
             </div>
         </div>
